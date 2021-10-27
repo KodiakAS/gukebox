31a17ed192069a765d19e233e6a90bb57fb4b970
KodiakAS <kodiakas@hotmail.com>
2021-10-27 16:05:40 +0800

    First version of parallel module

--- libs/parallel.sh
+++ libs/parallel.sh
@@ -1,3 +1,55 @@
 #!/usr/bin/env bash
 
+# Get file descriptor of current bash process
+function gb::get_self_fd() {
+    ls /proc/$$/fd
+}
 
+# Open a pipe, Only works on bash 4.1+
+#
+# Vars set:
+#   GB_FREE_FD
+function gb::mkpipe() {
+    gb::check_bash_version "4.1.0"
+    mkfifo pipe-$$
+    exec {GB_FREE_FD}<>pipe-$$
+    rm pipe-$$
+}
+
+# run the given command asynchronously and pop/push tokens
+function gb::run_with_lock() {
+    local job_file=${1}
+    local max_jobs=${2}
+    shift
+    shift
+    local running_jobs=0
+
+    gb::mkpipe
+
+    while IFS= read -r job; do
+        if [ -z "${job}" ]; then
+            continue
+        fi
+
+        if ((running_jobs < max_jobs)); then
+            running_jobs=$((running_jobs + 1))
+            echo "start a new job: $job"
+            ("$@" && echo "done") >&${GB_FREE_FD} &
+        else
+            IFS= read -r ans <&${GB_FREE_FD}
+            echo "job output: $ans"
+            echo "start a new job: $job"
+            ("$@" && echo "done") >&${GB_FREE_FD} &
+        fi
+    done <"${job_file}"
+
+    while IFS= read -r ans; do
+        running_jobs=$((running_jobs - 1))
+        echo "job output: $ans"
+        if ((running_jobs == 0)); then
+            break
+        fi
+    done <&${GB_FREE_FD}
+
+    exec {GB_FREE_FD}<&-
+}
