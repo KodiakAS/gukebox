1f2ca297f99379afad2b4028a8a114e32c3a989f
KodiakAS <kodiakas@hotmail.com>
2021-10-27 16:04:09 +0800

    Update init script

--- gukebox.sh
+++ gukebox.sh
@@ -8,10 +8,12 @@ set -o pipefail
 
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
+# Low level modules
 source "${CUR_DIR}/libs/common.sh"
-
 source "${CUR_DIR}/libs/logging.sh"
 source "${CUR_DIR}/libs/util.sh"
+
 source "${CUR_DIR}/libs/parallel.sh"
+source "${CUR_DIR}/libs/git.sh"
 
 gb::log::enable_error_handler
