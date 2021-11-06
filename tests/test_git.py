6a48eb93b84cfd9eca2aa95f67d2aca2dc296a7d
KodiakAS <kodiakas@hotmail.com>
2021-11-06 12:26:06 +0800

    Add missing cases

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,6 +1,19 @@
 import os
+import re
 
 
 def test_show_root(bash):
     bash.send("source ./gukebox.sh")
     assert bash.send('gb::git::show_root') == os.getcwd()
+
+
+def test_get_last_tag(bash):
+    bash.send("source ./gukebox.sh")
+    ret = bash.send("gb::git::get_last_tag")
+    assert re.fullmatch(r'\d\.\d\.\d', ret)
+
+
+def test_count_commits_ahead_last_tag(bash):
+    bash.send("source ./gukebox.sh")
+    ret = bash.send("gb::git::count_commits_ahead_last_tag")
+    assert re.fullmatch(r'\d', ret)
